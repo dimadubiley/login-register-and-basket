b2824522b536faacc11be66c820d1de844c1123b
Dima <Dima@gmail.com>
2025-06-09 11:57:01 +0300

    d

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login';
+import Register from './components/register';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Register/>
     </div>
   );
 }
---
+++ src/components/register.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './register.css';
+
+function Register() {
+    return (
+
+        <div id='register'>
+            <h1 id='titleRegister'>SWAGA</h1>
+
+            <div id='wrappRegister'>
+                <div id='leftRegister'>
+                    <h2 id='titleH2Register'>Register</h2>
+
+                    <div id='formRegister'>
+                        <div className='formRegister'>
+                            <input type="text" className='inputRegister' placeholder="E-Mail" />
+                        </div>
+                        <hr className='hrRegister' />
+                        <div className='formRegister'>
+                            <input type="password" className='inputRegister' placeholder="Password" />
+                        </div>
+                        <hr className='hrRegister' />
+                        <div className='formRegister'>
+                            <input type="text" className='inputRegister' placeholder="Name" />
+                        </div>
+                        <hr className='hrRegister' />
+                        <div className='formRegister'>
+                            <input type="text" className='inputRegister' placeholder="Surname" />
+                        </div>
+                        <hr className='hrRegister'/>
+
+
+                        <div className='agreeRegister'>
+                            <input type="checkbox" className='checkboxInAtreeRegister'></input>
+                            <p className='pInAgreeRegister'>I want to receive personalized commercial messages from NAME by email.</p>
+                        </div>
+                        <div className='agreeRegister'>
+                            <input type="checkbox" className='checkboxInAtreeRegister'></input>
+                            <p className='pInAgreeRegister'>I have read and understand the privacy policy and use of cookies</p>
+                        </div>
+
+                        <button id='bntRegister'>Sing in</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
