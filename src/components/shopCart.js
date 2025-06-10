a2c237cb89a8b5ec8a6d86641ff1ff4bfe0f3295
Dima <Dima@gmail.com>
2025-06-10 13:20:21 +0300

    dd

--- src/components/shopCart.js
+++ src/components/shopCart.js
@@ -1,6 +1,33 @@
 import React from 'react';
 import './shopCart.css';
-import Cart from './cartForShopCart';
+import './cartForShopCart.css';
+// import {Cart, CartFavorite}  from './cartForShopCart';
+function Cart() {
+    return (
+        <div className='cartShopCart'>
+            <img src='Flacone.png' alt='' className='fhotoForShopCart' />
+            <div className='cartContentShopCart'>
+                <p>PARFUMATED WATER RED MYRA TEMPTATION</p>
+                <p>499,00 UAH</p>
+            </div>
+        </div>
+    );
+}
+function CartFavorite() {
+    return (
+        <div className='cartInFaoriteShopCart'>
+            <img src='Flacone.png' alt='' className='fhotoForShopCart' />
+            <p>ДЖИНСИ TRF WIDE-LEG З ВИСОК</p>
+            <p className='priceFavoriveCart'>1 799,00 UAH</p>
+
+            <div className='countTovarShopCart'>
+                <p className='pForContTovarShopCart'>-</p>
+                <p className='pForContTovarShopCart'>1</p>
+                <p className='pForContTovarShopCart'>+</p>
+            </div>
+        </div>
+    );
+}
 function ShopCart() {
     return (
         <div id='wrappShopCart'>
@@ -10,32 +37,37 @@ function ShopCart() {
                 <img src='Bookmark.png' alt='' id='bookmarkShopCart' />
             </div>
             <div id='divUnderNavShopCart'>
-                <img src='cartshopCart.png' alt='' id='imgcartshopCart' />
-                <p>your cart is empty</p>
+
+                <CartFavorite/>
+                <CartFavorite/>
+                <CartFavorite/>
+                <CartFavorite/>
+                <CartFavorite/>
+
             </div>
 
             <div id='divForProportisShopCart'>
                 <p>can also hit you up</p>
 
                 <div id='cartsShopCart'>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
-                <Cart/>
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
+                    <Cart />
                 </div>
             </div>
         </div>
