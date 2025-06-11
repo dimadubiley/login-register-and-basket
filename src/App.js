e6eaa0de2d35845adb2df42d51ae96f2db164682
Dima <Dima@gmail.com>
2025-06-11 14:14:55 +0300

    d

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import ShopCart from './components/shopCart';
 function App() {
   return (
     <div className="App">
-      {/* <Register/> */}
-      <Login/>
+      <Register/>
+      {/* <Login/> */}
       {/* <ShopCart/> */}
     </div>
   );
